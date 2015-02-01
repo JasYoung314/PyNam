569e606e8baa4dd95d78d00ec399391bcd5c59ca
vince <vincent.knight@gmail.com>
2015-02-01 16:33:09 +0200

    Have a random player

--- Workshop/axelrod.py
+++ Workshop/axelrod.py
@@ -4,6 +4,7 @@ Recreate Axelrod's tournament
 
 """
 import itertools
+import random
 
 class Axelrod:
     """
@@ -200,6 +201,51 @@ class Cooperator(Player):
         >>>
         """
         return 'C'
+
+    def __repr__(self):
+        """
+i       The string method for the strategy:
+
+            >>> P1 = Cooperator()
+            >>> print P1
+            Cooperator
+        """
+        return 'Cooperator'
+
+class Random(Player):
+    """
+    A player who randomly chooses between cooperating and defecting
+    """
+    def strategy(self, opponent):
+        """
+        Always returns 'C'
+
+            >>> random.seed(1)
+            >>> P1 = Random()
+            >>> P2 = Player()
+            >>> P1.strategy(P2)
+            'C'
+
+        This is not affect by the history of either player:
+
+            >>> random.seed(1)
+            >>> P1.history = ['C', 'D', 'C']
+            >>> P2  = ['C', 'C', 'D']
+            >>> P1.strategy(P2)
+            'C'
+
+        It is simply a random choice:
+
+            >>> random.seed(2)
+            >>> P1.strategy(P2)
+            'D'
+            >>> P1.strategy(P2)
+            'D'
+            >>> P1.strategy(P2)
+            'C'
+        >>>
+        """
+        return random.choice(['C','D'])
 
     def __repr__(self):
         """
