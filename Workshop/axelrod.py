2a4781c464e91dd0e8c1452eb27ec4d0c07bc711
vince <vincent.knight@gmail.com>
2015-02-01 14:13:09 +0200

    Tests that fail and updated gitignore

--- Workshop/axelrod.py
+++ Workshop/axelrod.py
@@ -1,4 +1,33 @@
 """
 Recreate Axelrod's tournament
+
+
 """
 
+class Axelrod:
+    """
+    A class for an iterated prisoner's dilemma.
+
+    Take a list of players (see the Player class):
+
+        >>> P1 = Defector()
+        >>> P2 = Cooperator()
+        >>> axelrod = Axelrod(P1, P2)
+        >>> axelrod.round_robin(turns=10)
+        >>> for player in sorted(axelrod.players, key=lambda x: x.score):
+        ...     player.name, player.score
+        P1, 0
+        P2, 20
+    """
+    def __init__(self, *args):
+        """
+        Initiate a tournament of players:
+
+            >>> P1 = Defector()
+            >>> P2 = Defector()
+            >>> P3 = Defector()
+            >>> axelrod = Axelrod(P1, P2, P3)
+            >>> axelrod.players
+            [Defector, Defector, Defector]
+        """
+        self.players = list(args)
